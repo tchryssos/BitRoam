b253e7bf702ae78f7631bae1400e71135cb0d90d
Troy Chryssos <github email address>
2016-11-11 18:23:37 -0500

    holding arrow keys triggers proper animation, although it studders and repeats

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 $(document).ready(()=>{
 
+  let stepRight="<img src='./art/stepping right.png' id='step-right'/>"
+
+  let lookRight="<img src='./art/looking right.png' id='look-right'/>"
+
+  let stepLeft="<img src='./art/stepping left.png' id='step-left'/>"
+
+  let lookLeft="<img src='./art/looking left.png' id='look-left'/>"
+
+  let keyPressed=null
+
+  $("#boy-walking").append(lookRight)
+
   $('body').keydown((e)=>{
     $("#boy-walking").empty()
     if(e.which===39){
-      $("#boy-walking").append("<img src='./art/looking right.png'/>")
+      $("#boy-walking").append(lookRight)
+      if (keyPressed) return
+      keyPressed=setInterval(()=>{
+        if ($("#boy-walking").children()[0].id==="look-right"){
+          $("#boy-walking").empty()
+          $("#boy-walking").append(stepRight)
+        } else {
+          $("#boy-walking").empty()
+          $("#boy-walking").append(lookRight)
+        }
+      }, 250)
+
     } else if(e.which===37){
-      $("#boy-walking").append("<img src='./art/looking left.png'/>")
+      $("#boy-walking").append(lookLeft)
+      if (keyPressed) return
+      keyPressed=setInterval(()=>{
+      if ($("#boy-walking").children()[0].id==="look-left"){
+        $("#boy-walking").empty()
+        $("#boy-walking").append(stepLeft)
+      } else {
+        $("#boy-walking").empty()
+        $("#boy-walking").append(lookLeft)
+      }
+      }, 250)
     }
   })
 
   $('body').keyup((e)=>{
     $("#boy-walking").empty()
     if(e.which===39){
-      $("#boy-walking").append("<img src='./art/looking right.png'/>")
+      clearInterval(keyPressed)
+      keyPressed=null
+      $("#boy-walking").append(lookRight)
+
     } else if (e.which===37){
-      $("#boy-walking").append("<img src='./art/looking left.png'/>")
+      clearInterval(keyPressed)
+      keyPressed=null
+      $("#boy-walking").append(lookLeft)
     }
   })
+
+
 })
