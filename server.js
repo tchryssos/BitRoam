c051d963fc6dafeb9c3016716feb0e54c52767a7
Troy Chryssos <github email address>
2016-11-16 15:14:46 -0500

    navigated app to node.js to allow for backend. Will use backend to send weather API requests for now

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require('express')
+var morgan = require('morgan')
+var bodyParser = require('body-parser')
+var app = express()
+
+app.use(morgan('dev'))
+app.use('/static', express.static('public'))
+
+app.set('views', './views')
+app.set('view engine','ejs')
+
+app.get('/', function(req, res){
+  res.render('index')
+})
+
+app.listen(3000, (err)=>{
+  if (err) console.log("could not start server")
+  if (!err)console.log("Server is running on port 3000")
+})
